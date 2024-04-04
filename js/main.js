08f34a0319a88cb3a648a192c63cc9e36d37f0e2
Lintra12 <uko.don@gmail.com>
2024-04-04 17:45:56 +0300

    add project

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+$(function () {
+
+  $("body").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+  var mixer = mixitup('.portfolio__content');
+  
+    
+});
+
+$(function () {
+  const menu = document.querySelector(".menu")
+
+  window.addEventListener("scroll", function () {
+    const scrollPos = window.scrollY
+
+    if (scrollPos > 90) {
+      menu.classList.add('fixed')
+    } else {
+      menu.classList.remove('fixed')
+    }
+  });
+});
+
+
+$('.menu__btn, .menu__link').on('click', function () {
+  $('.menu__btn').toggleClass('btn-cross');
+  $('.menu__list').toggleClass('menu-open');
+  $('.body').toggleClass('lock')
+});
+
+$('.menu__link').on('click', function () {
+  $('.menu__btn').removeClass('btn-cross');
+  $('.menu__list').removeClass('menu-open');
+  $('.body').removeClass('lock')
+});
